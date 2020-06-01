a1001ec846320d6753591f3e7e194193d4a5c5a5
Shahar Mintz <smintz@protoconf.com>
2020-06-01 15:13:31 +0300

    adding devservers in loadbalancer

--- cmd/balancer/main.go
+++ cmd/balancer/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/smintz/similarbalancer/balancer"
+	"github.com/smintz/similarbalancer/webserver"
 )
 
 type BackendsArray []*balancer.Backend
@@ -25,13 +27,23 @@ func (i *BackendsArray) Set(value string) error {
 }
 
 var (
-	ADDR     = flag.String("addr", ":10000", "Listen address for load balancer")
-	BACKENDS = BackendsArray{}
+	ADDR        = flag.String("addr", ":10000", "Listen address for load balancer")
+	BACKENDS    = BackendsArray{}
+	testServers = flag.Int("devservers", 0, "number of webservers to add")
 )
 
 func main() {
 	flag.Var(&BACKENDS, "b", "backend address (can repeat)")
 	flag.Parse()
+
+	for i := 1; i < *testServers; i++ {
+		dir, _ := ioutil.TempDir("/tmp", "similar")
+		url := fmt.Sprintf("localhost:2000%d", i)
+		w := webserver.NewWebServer(dir, url, uint(i)*1)
+		go w.Serve()
+		BACKENDS = append(BACKENDS, balancer.NewBackend("http://"+url))
+		log.Println("added new backend", w)
+	}
 	log.Println("starting balancer at", *ADDR, BACKENDS.String())
 	pool := balancer.NewServerPool(BACKENDS)
 
